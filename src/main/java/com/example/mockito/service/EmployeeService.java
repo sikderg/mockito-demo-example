509cbd5702a220594a4be89956ad6cfcd2de430c
Goutam Sikder <goutam.sikder@talentmsh.com>
2022-04-18 23:07:10 +0530

    Mock Test Service Layer

---
+++ src/main/java/com/example/mockito/service/EmployeeService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.mockito.service;
+
+import com.example.mockito.model.Employee;
+
+/**
+ * @author Goutam Sikder
+ *
+ */
+public interface EmployeeService {
+	
+	Employee saveEmployee(Employee employee);
+
+}
